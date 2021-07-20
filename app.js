e4f65fc3bfd37bc772f5089f3109a639bd524f64
Krupali Patel <krupali810@yahoo.com>
2021-07-19 23:54:38 -0400

    code cleanup

--- app.js
+++ app.js
@@ -225,13 +225,3 @@ submitButton.addEventListener("click", function () {
   const resultGridView = createTilesGrid(humanInfoTile);
   displayGrid(resultGridView);
 });
-
-// Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-// Generate Tiles for each Dino in Array
-// Add tiles to DOM
-
-// Remove form from screen
-
-// On button click, prepare and display infographic
